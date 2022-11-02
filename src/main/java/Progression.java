3676d7da71b3e7ba524d61c3e7235ff216d57a1d
Ilya Danilov <esozap32@mail.ru>
2022-11-02 23:40:40 +0400

    9859659

---
+++ src/main/java/Progression.java
@@ -0,0 +1,9 @@
+public class Progression {
+    public static void main(String[] args) {
+       int b = 0;
+        do {
+           for (int i = 0; i <= 100; i++) {
+               System.out.println(i);
+           }
+       }while ( b>=100);
+    }
