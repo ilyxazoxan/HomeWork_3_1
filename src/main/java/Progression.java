e3bc929407484c0e1f71647716cf452fe99e3faf
Ilya Danilov <esozap32@mail.ru>
2022-11-05 20:18:49 +0400

    SimpleLoop

--- src/main/java/Progression.java
+++
@@ -1,9 +0,0 @@
-public class Progression {
-    public static void main(String[] args) {
-       int b = 0;
-        do {
-           for (int i = 0; i <= 100; i++) {
-               System.out.println(i);
-           }
-       }while ( b>=100);
-    }
--- src/main/java/SimpleLoop.java
+++ src/main/java/SimpleLoop.java
@@ -1,8 +1,10 @@
 public class SimpleLoop {
     public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("i = " + i);
 
-        for (int i = 0; i <= 100; i++) {
-            System.out.println(i);
         }
     }
 }
+
+
